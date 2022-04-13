dda096c55f7b1469fc74f6bfc0c57abaf948eeda
Curtis Maher <18maherc@gmail.com>
2022-04-13 00:52:51 -0400

    almost done with arduino code

--- main/main.ino
+++ main/main.ino
@@ -9,14 +9,20 @@
 #define SD_MOSI 11 // SPI MOSI
 #define SD_CS 4    // SPI chip select
 
-enum ternary
+typedef enum actions
 {
   tip,
   ring,
   pull
 };
+typedef enum responses
+{
+  correct,
+  incorrect,
+  nothing
+};
 
-ternary action;
+actions action;
 byte rann;
 bool gameOver;
 byte score;
@@ -51,38 +57,47 @@ void loop()
     if (rann == 1)
     {               // Checking and assigning enum action
       action = tip; // Assigning enum action
+      // TODO: output to speaker
+      // TODO: ??maybe flash current action onto screen??
     }
     else if (rann == 2)
     {                // Checking and assigning enum action
       action = ring; // Assigning enum action
+      // TODO: output to speaker
+      // TODO: ??maybe flash current action onto screen??
     }
     else
     {                // Checking and assigning enum action
       action = pull; // Assigning enum action
+      // TODO: output to speaker
+      // TODO: ??maybe flash current action onto screen??
     }
+
     checkSuccess(interval, action); // Time to see if user performs action
-    if ((score % 4) == 0)           // Check if it's time to speed up
-      interval = interval - 100;    // Decreasing time interval
+
+    if ((score % 4) == 0)        // Check if it's time to speed up
+      interval = interval - 100; // Decreasing time interval
+
+    // TODO: Update LCD with current score
   }
 }
 
-void checkSuccess(int timelimit, ternary actiontype)
-{                // Check objective sequence
-  tiltCount = 0; // sets tilt count to 0
-
+void checkSuccess(int timelimit, actions actiontype) // Check objective sequence
+{
   for (int count = 0; count < timelimit; count++)
-  { // Iter                   // Setting based on input
-    if (checkInput(actiontype) == 1)
-    {                 // Check if user performed action
+  {
+    responses inputresponse = checkInput(actiontype); // Setting based on input
+    if (inputresponse == correct)                     // Check if user performed action
+    {
       continu = true; // Proceed with game
       score++;        // Increment user's score
       return;
     }
-    else if (checkInput(actiontype) == 2)
-    { // Check if user performed wrong action
+    else if (inputresponse == incorrect) // Check if user performed wrong action
+    {
       break;
     }
-    else
+    else if (inputresponse == nothing) // Check if user hasn't done anything
     {
       continue;
     }
@@ -91,91 +106,111 @@ void checkSuccess(int timelimit, ternary actiontype)
   fail(); // Have not completed in time so run end-game
 }
 
-int checkInput(ternary actiontype)
-{                           // Check input signal(s)
-  int xval = analogRead(x); // Reading joysticks for whole checkInput
-  int yval = analogRead(y);
-
-  if (actiontype == tip)
-  { // Tilt
-    if (digitalRead(tiltsw))
-    { // Check if they tilt for 50
-      tiltCount++;
-      if (tiltCount == 50)
-      {
-        return 1;
-      }
-    }
-    else if (digitalRead(udder))
-    { // If any other action return false
-      return 2;
-    }
-    else if (xval > 600 || xval < 400 || yval > 600 || yval < 400)
+responses checkInput(actions actiontype) // Check input signal(s)
+{
+  if (actiontype == tip) // Tilt action
+  {
+    if (checkTip()) // Check our tilt first
     {
-      return 2;
+      return correct;
     }
-    else
+    else if (checkUdder()) // Check other input actions
     {
-      return 0;
+      return incorrect;
+    }
+    else if (checkRing()) // Check other input actions
+    {
+      return incorrect;
+    }
+    else // Nothing was done
+    {
+      return nothing;
     }
   }
-  else if (actiontype == ring)
-  { // Ring
-    if (xval > 600 || xval < 400 || yval > 600 || yval < 400)
-    { // Check if joystick is moved far enough
-      return 1;
-    }
-    else if (digitalRead(tilt))
+  else if (actiontype == ring) // Ring action
+  {
+    if (checkRing()) // Check our ring first
     {
-      tiltCount++; // If any other action return false
-      if (tiltCount == 50)
-      {
-        return 2;
-      }
+      return correct;
     }
-    else if (digitalRead(udder))
+    else if (checkTip()) // Check other input actions
     {
-      return 2;
+      return incorrect;
     }
-    else
+    else if (checkUdder()) // Check other input actions
     {
-      return 0;
+      return incorrect;
+    }
+    else // Nothing was done
+    {
+      return nothing;
     }
   }
-  else if (actiontype == pull)
-  { // Udder
-    if (!digitalRead(udder))
-    { // Check if udder is yanked
-      return 1;
-    }
-    else if (digitalRead(tilt))
-    { // If any other action return false
-      tiltCount++;
-      if (tiltCount == 50)
-      {
-        return 2;
-      }
-    }
-    else if (xval > 600 || xval < 400 || yval > 600 || yval < 400)
+  else if (actiontype == pull) // Pull action
+  {
+    if (checkUdder()) // Check our pull first
     {
-      return 2;
+      return correct;
     }
-    else
+    else if (checkTip()) // Check other input actions
     {
-      return 0;
+      return incorrect;
+    }
+    else if (checkRing()) // Check other input actions
+    {
+      return incorrect;
+    }
+    else // Nothing was done
+    {
+      return nothing;
     }
   }
 }
 
-void startup()
-{ // Startup sequence at beginning of game
-  // TODO count 3, 2, 1 and blink LEDs or smth
+bool checkUdder()
+{
+  return !digitalRead(udder); // Check if udder is yanked
+}
+
+bool checkTip()
+{
+  for (int i = 0; i < 2000; i++) // 2 seconds to correct tilt
+  {
+    if (!digitalRead(tiltsw))
+    {
+      return false; // tilt switch is not activated
+    }
+    else
+    {
+      // TODO: output message to LCD that tilt switch is activated
+    }
+  }
+  return true; // tilt was activated for long enough to be considered an input
+}
+
+bool checkRing()
+{
+  int xval = analogRead(x);                                 // Reading joystick x value
+  int yval = analogRead(y);                                 // Reading joystick y value
+  if (xval > 600 || xval < 400 || yval > 600 || yval < 400) // Check if moved far enough
+  {
+    return true; // Joystick was moved far enough
+  }
+  else
+  {
+    return false; // Joystick was NOT moved far enough
+  }
+}
+
+void startup() // Startup sequence at beginning of game
+{
+  // TODO count 3, 2, 1
   // TODO set 7-seg to "go"
   continu = true; // Proceeding with the game
 }
 
-void fail()
-{                  // Ending sequence to finish game
+void fail() // Ending sequence to finish game
+{
   continu = false; // Cannot proceed with the game
   // TODO blink final score
   // TODO set 7 seg to 00 or dash dash or smth
